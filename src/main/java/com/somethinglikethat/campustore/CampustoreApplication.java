01783b6787eb3fe2c81c829c2824b77e87af9c55
replaykim <replaykim123@gmail.com>
2017-08-20 14:23:13 +0900

    init

---
+++ src/main/java/com/somethinglikethat/campustore/CampustoreApplication.java
@@ -0,0 +1,12 @@
+package com.somethinglikethat.campustore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampustoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampustoreApplication.class, args);
+	}
+}
